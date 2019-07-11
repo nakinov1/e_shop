97bbec790a253a463d4f3333b14a0327921e4c4e
nakinov1 <48454318+nakinov1@users.noreply.github.com>
2019-07-11 21:17:30 +0200

    Update url

--- scripts/cart.js
+++ scripts/cart.js
@@ -4,7 +4,7 @@ var cartTotalList = document.getElementById('cartTotalList');
 var cartTotal = document.getElementById('cartTotal');
 
 function getMyProducts() {
-  fetch('http://myjson.com/o7cjb')
+  fetch('https://github.com/nakinov1/e_shop/blob/master/json/myCartProducts.json')
   .then(function(response) {
     return response.json();
   })
